090a523dc76de1425255e1016e5f9ee1c0ad5291
Domenico Giambra <sviluppo@editt.it>
2019-08-27 10:13:56 +0200

    Every cache refresh wipes the whole cache folder

--- Config.php
+++ Config.php
@@ -23,8 +23,27 @@ $this->cache = [];
 	 */
 	function makeCache(): bool
 	{
-		return (bool)file_put_contents(INCLUDE_PATH . 'model' . DIRECTORY_SEPARATOR . 'Output' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'cache.php', '<?php
-$this->cache = [];
-');
+		return (
+			(bool)file_put_contents(INCLUDE_PATH . 'model' . DIRECTORY_SEPARATOR . 'Output' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'cache.php', "<?php\n\$this->cache = [];\n")
+			and $this->delTree(INCLUDE_PATH . 'model' . DIRECTORY_SEPARATOR . 'Output' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'cache')
+		);
+	}
+
+	/**
+	 * Removes entire directory
+	 *
+	 * @param string $dir
+	 * @return bool
+	 */
+	private function delTree(string $dir): bool
+	{
+		if (!is_dir($dir))
+			return true;
+
+		$files = array_diff(scandir($dir), ['.', '..']);
+		foreach ($files as $file) {
+			(is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file");
+		}
+		return rmdir($dir);
 	}
 }
